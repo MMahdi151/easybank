0e8f81fdcd7095f37dbc7477698efadede3e7779
Mahdi <mmahdikarimi2006@gmail.com>
2024-09-13 13:35:12 +0430

    hero-section completed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
-    <section className="h-screen flex flex-col-reverse lg:flex-row  max-w-6xl">
-      <div className="flex flex-col gap-8 lg:gap-12 pt-12 px-6 lg:p-0 text-center lg:text-left lg:place-content-center lg:w-1/2 h-1/2 lg:h-full">
-        <h1 className="text-5xl lg:text-7xl">
+    <section className="h-screen flex flex-col-reverse md:flex-row w-screen max-w-7xl">
+      <div className="flex flex-col gap-8 md:gap-12 pt-12 px-6 md:p-0 md:pr-[5%] text-center md:text-left md:place-content-center md:w-1/2 h-1/2 md:h-full">
+        <h1 className="text-5xl md:text-7xl">
           Next generation digital banking
         </h1>
         <p>
@@ -16,24 +16,46 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <div className="relative bottom-0 w-full lg:w-1/2 h-1/2 lg:h-full">
+      <div className="w-full md:w-1/2 h-1/2 md:h-full relative md:static overflow-hidden">
+        <img
+          src="/images/bg-intro-desktop.svg"
+          className="absolute z-0 h-full left-1/2 -translate-x-1/2 scale-95 md:scale-100 md:h-auto md:min-w-[1440px] md:top-1/2 md:-translate-y-[54%] md:left-[44%] md:translate-x-0"
+        />
+        <img
+          src="/images/image-mockups.png"
+          className="absolute z-20 h-full md:h-auto left-1/2 -translate-x-1/2  md:scale-[115%] md:top-6 md:left-[62%] md:translate-x-0"
+        />
+      </div>
+      {/* <div className="md:w-1/2 h-1/2 md:h-full relative z-20">
+        <img
+          src="/images/bg-intro-desktop.svg"
+          className="scale-[200%] ml-[24%] lg:ml-[36%] lg:mt-[16%] absolute z-0"
+        />
+        <img
+          src="/images/image-mockups.png"
+          className="absolute lg:scale-[120%] w-full -top-[4%] lg:top-[8%] lg:left-[28%] z-50"
+        />
+      </div> */}
+      {/* <div className="w-full lg:w-1/2  h-1/2 lg:h-full overflow-hidden">
+        <div className="">
+          <img
+            src="/images/bg-intro-desktop.svg"
+            className="w-full md:absolute top-[44%] md:-translate-y-1/2 left-1/2 md:-translate-x-[5%]"
+          />
+          <img
+            src="/images/image-mockups.png"
+            className="md:w-[56%] md:absolute top-[56%] -translate-y-[72%] md:-translate-y-1/2 left-[60%] z-20"
+          />
+        </div>
+      </div> */}
+      {/* <div className="relative bottom-0 w-full lg:w-1/2 h-1/2 lg:h-full">
         <img
           src="/images/bg-intro-desktop.svg"
           className="min-w-[240%] -translate-x-[14%] lg:-translate-x-14 absolute -bottom-12 lg:min-w-[260%] lg:-bottom-28"
         />
         <img
           src="/images/image-mockups.png"
-          className="absolute bottom-0 lg:min-w-[160%] md:right-6 lg:left-28 lg:-top-6 z-20"
-        />
-      </div>
-      {/* <div className="lg:w-1/2 top-0 lg:relative max-h-1/2">
-        <img
-          src="/images/bg-intro-desktop.svg"
-          className="min-w-[200%] lg:min-w-[244%] -mt-40 lg:-mt-[32%] -ml-36 -ml-1/2"
-        />
-        <img
-          src="/images/image-mockups.png"
-          className="min-w-[] lg:min-w-[150%] absolute -top-12 left-[20%] z-20"
+          className="absolute bottom-0 lg:min-w-[160%] md:right-6 lg:left-28 lg:-top-6 z-10"
         />
       </div> */}
     </section>
